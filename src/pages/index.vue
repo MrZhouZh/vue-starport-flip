37db007c14e41a579acb0223b0008cdecd184afb
MrZhouZh <17600330793@sina.cn>
2023-08-10 14:38:43 +0800

    feat: init repo

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+const router = useRouter()
+
+function go() {
+  console.log('click--')
+
+  router.push('/foo')
+}
+</script>
+
+<template>
+  <div>
+    <div>Index</div>
+
+    <div>
+      <button
+        class="m-3 text-sm btn"
+        @click="go"
+      >
+        Go
+      </button>
+    </div>
+  </div>
+</template>
